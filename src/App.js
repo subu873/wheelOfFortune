d278790b49f859bc5d251fc5a3468f5479c0901a
shubham <singhshubham873@gmail.com>
2022-02-23 11:06:37 +0530

    wheel of fortune complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {Wheel} from 'react-custom-roulette'
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [spin, setSpin] = useState(false)
+    const data = [
+        {option: '0', style: {backgroundColor: 'green', textColor: 'black'}},
+        {option: '1', style: {backgroundColor: 'white'}},
+        {option: '2'},
+    ]
+
+    const handleCompleteSpin = () => {
+        alert("complete spin")
+    }
+
+    const handleStartSpin = () => {
+        setSpin(true)
+    }
+
+    return (
+        <div>
+            <Wheel
+                mustStartSpinning={spin}
+                prizeNumber={1} // this is winner index of data
+                data={data}
+                backgroundColors={['#3e3e3e', '#df3428']}
+                textColors={['#ffffff']}
+                onStopSpinning={handleCompleteSpin}
+            />
+            <button className="button" onClick={handleStartSpin} style={{color:"red",padding:15,fontSize:25}}>
+                Start Spin
+            </button>
+        </div>
+    );
 }
 
 export default App;
